af845745f486c7c6c0475400df4b62a5b5d77ba4
overetou <overetou@student.42.fr>
2020-07-02 18:51:35 +0200

    now we have reasonbly sized chunks of code

--- src/md5_execute_loaded.c
+++ src/md5_execute_loaded.c
@@ -17,51 +17,46 @@ void	md5_set_end_bits_len(unsigned char *adr, unsigned long val)
 	}
 }
 
-void	md5_execute_loaded(t_master *m)
+void	md5_exec_final_funcs(t_master *m)
+{
+	int i;
+
+	i = 0;
+	while (i != m->final_funcs_number)
+	{
+		m->final_exec_funcs[i](m);
+		i++;
+	}
+	free(m->final_exec_funcs);
+}
+
+void	exec_files_hash_and_funcs(t_master *m)
 {
 	int	i;
+
+	i = 0;
+	while (i != m->files_to_hash_nb)
+	{
+		exec_file_hash(m, m->files_to_hash[i]);
+		i++;
+	}
+	free(m->files_to_hash);
+	if (m->final_funcs_number)
+		md5_exec_final_funcs(m);
+}
+
+void	md5_execute_loaded(t_master *m)
+{
 	unsigned char	*md5sum;
 
 	if (m->files_to_hash_nb)
-	{
-		// putstr("Inside file hash branch\n");
-		i = 0;
-		while (i != m->files_to_hash_nb)
-		{
-			exec_file_hash(m, m->files_to_hash[i]);
-			i++;
-		}
-		free(m->files_to_hash);
-		if (m->final_funcs_number)
-		{
-			// printf("Inside file hash branch, executing special tasks.\n final_funcs_number: %d\n", m->final_funcs_number);
-			i = 0;
-			while (i != m->final_funcs_number)
-			{
-				m->final_exec_funcs[i](m);
-				i++;
-			}
-			free(m->final_exec_funcs);
-		}
-	}
+		exec_files_hash_and_funcs(m);
 	else
 	{
-		// putstr("No files to hash detected\n");
 		if (m->final_funcs_number)
-		{
-			// printf("executing special tasks.\n final_funcs_number: %d\n", m->final_funcs_number);
-			i = 0;
-			while (i != m->final_funcs_number)
-			{
-				// printf("i = %d. p_exec adr = %p, current adr = %p\n", i, md5_p_exec, (m->final_exec_funcs)[i]);
-				((m->final_exec_funcs)[i])(m);
-				i++;
-			}
-			free(m->final_exec_funcs);
-		}
+			md5_exec_final_funcs(m);
 		else
 		{
-			// putstr("Nothing given to hash. Reading from stdin.\n");
 			load_stdin(m);
 			md5sum = md5_digest(m->stdin_string);
 			print_checksum(md5sum);
--- src/md5_final_exec.c
+++ src/md5_final_exec.c
@@ -43,10 +43,35 @@ void	md5_s_final_exec(t_master *m)
 	free(md5sum);
 }
 
+void	display_file_content_hash(t_master *m, char *file_content, char *file_name)
+{
+	unsigned char	*md5sum;
+
+	md5sum = md5_digest(file_content);
+	if (m->quiet_enabled)
+		print_checksum(md5sum);
+	else
+	{
+		if (m->reverse_enabled)
+		{
+			print_checksum(md5sum);
+			putstr(" ");
+			putstr(file_name);
+		}
+		else
+		{
+			putstr("MD5 (");
+			putstr(file_name);
+			putstr(") = ");
+			print_checksum(md5sum);
+		}
+	}
+	free(md5sum);
+}
+
 void	exec_file_hash(t_master *m, char *file_name)
 {
 	char			*file_content;
-	unsigned char	*md5sum;
 	int 			fd;
 
 	fd = open(file_name, O_RDONLY);
@@ -59,27 +84,8 @@ void	exec_file_hash(t_master *m, char *file_name)
 	{
 		load_file(m, fd, &file_content);
 		close(fd);
-		md5sum = md5_digest(file_content);
-		if (m->quiet_enabled)
-			print_checksum(md5sum);
-		else
-		{
-			if (m->reverse_enabled)
-			{
-				print_checksum(md5sum);
-				putstr(" ");
-				putstr(file_name);
-			}
-			else
-			{
-				putstr("MD5 (");
-				putstr(file_name);
-				putstr(") = ");
-				print_checksum(md5sum);
-			}
-		}
+		display_file_content_hash(m, file_content, file_name);
 		free(file_content);
-		free(md5sum);
 	}
 	putstr("\n");
 }
